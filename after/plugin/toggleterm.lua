fa3ac2f98e8459b409176ba7195fe4611b2117d5
waterkhair <jesus.barajas@outlook.com>
2023-11-08 02:23:49 -0800

    Fixing terminal plugin.

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,9 +1,26 @@
 require('toggleterm').setup({
+    highlights = {
+        Normal = { link = 'Normal' },
+        NormalNC = { link = 'NormalNC' },
+        NormalFloat = { link = 'NormalFloat' },
+        FloatBorder = { link = 'FloatBorder' },
+        StatusLine = { link = 'StatusLine' },
+        StatusLineNC = { link = 'StatusLineNC' },
+        WinBar = { link = 'WinBar' },
+        WinBarNC = { link = 'WinBarNC' },
+    },
     size = function(term)
         if (term.direction == 'horizontal') then return 20
         elseif (term.direction == 'vertical') then return 15 end
     end,
+    on_create = function()
+        vim.opt.foldcolumn = '0'
+        vim.opt.signcolumn = 'no'
+    end,
     open_mapping = [[<c-\>]],
+    shading_factor = 2,
+    direction = 'float',
+    float_opts = { border = 'rounded' },
 })
 function _G.set_terminal_keymaps()
     local opts = { buffer = 0 }
@@ -17,7 +34,7 @@ function _G.set_terminal_keymaps()
     vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 end
 
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
+--[[-- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 local Terminal = require("toggleterm.terminal").Terminal
@@ -28,7 +45,7 @@ function _FLOAT_TOGGLE() float:toggle() end
 function _NODE_TOGGLE() node:toggle() end
 
 vim.keymap.set('n', '<leader>ft', '<cmd>lua _FLOAT_TOGGLE()<CR>')
-vim.keymap.set('n', '<leader>nt', '<cmd>lua _NODE_TOGGLE()<CR>')
+vim.keymap.set('n', '<leader>nt', '<cmd>lua _NODE_TOGGLE()<CR>')--]]
 
 --[[local whichkey = require('which-key')
 
