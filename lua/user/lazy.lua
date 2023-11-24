0821ff134dc4e7f438b9738069c04e85fea3ebba
waterkhair <jesus.barajas@outlook.com>
2023-11-23 21:11:17 -0800

    Adding keymaps for tabs and updating keymaps for color schemes.

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -25,7 +25,6 @@ require('lazy').setup({
     --{ 'glepnir/dashboard-nvim' }, -- Fancy and Blazing Fast start screen plugin of neovim
     --{ 'simrat39/symbols-outline.nvim' }, -- A tree like view for symbols in Neovim using the LSP
     --{ 'Exafunction/codeium.nvim' }, -- Native Codeium plugin for Neovim
-    --{ 'gbprod/yanky.nvim' }, -- Improve yank and put functionalities for Neovim
     --{ 'theHamsta/nvim-dap-virtual-text' }, -- Adds virtual text support to nvim-dap. nvim-treesitter is used to find variable definitions
     --{ 'jay-babu/mason-nvim-dap.nvim' }, -- Bridges mason.nvim with the nvim-dap plugin - making it easier to use both plugins together
     --{ 'folke/edgy.nvim' }, -- Easily create and manage predefined window layouts, bringing a new edge to your workflow
@@ -35,7 +34,9 @@ require('lazy').setup({
     --{ 'ahmedkhalf/project.nvim' }, -- An all in one neovim plugin written in lua that provides superior project management
     --{ 'mfussenegger/nvim-lint' }, -- An asynchronous linter plugin for Neovim (>= 0.6.0) complementary to the built-in LSP support
     --{ 'voldikss/vim-floaterm' }, -- Use (neo)vim terminal in the floating/popup window
-    --require('plugins.toggleterm'), -- A neovim plugin to persist and toggle multiple terminals
+    --{ 'plugins.toggleterm' }, -- A neovim plugin to persist and toggle multiple terminals
+    --{ 'tzachar/cmp-tabnine' }, -- Tabnine source for hrsh7th/nvim-cmp
+    --{ 'gbprod/yanky.nvim', opts = {}, lazy = true }, -- Improve yank and put functionalities for Neovim
 
     -- Themes
     require('plugins.catppuccin'), -- Catppuccin for (Neo)vim
@@ -60,25 +61,24 @@ require('lazy').setup({
     { 'saadparwaiz1/cmp_luasnip' }, -- luasnip completion source for nvim-cmp.
     { 'rafamadriz/friendly-snippets' }, -- Snippets collection for a set of different programming languages
     { 'nvimtools/none-ls.nvim', lazy = true }, -- null-ls.nvim Reloaded, maintained by the community
-    --{ 'tzachar/cmp-tabnine' }, -- Tabnine source for hrsh7th/nvim-cmp
     --{ 'NMAC427/guess-indent.nvim' }, -- Blazing fast indentation style detection for Neovim written in Lua
     --{ 'lukas-reineke/indent-blankline.nvim' }, -- Adds indentation guides to Neovim
 
     -- Coding
     { 'neovim/nvim-lspconfig', lazy = true }, -- Configs for the Nvim LSP client
     { 'williamboman/mason-lspconfig.nvim', lazy = true }, -- Bridges mason.nvim with the lspconfig plugin
+    { 'numToStr/Comment.nvim', opts = {} }, -- Smart and Powerful commenting plugin for neovim
     require('plugins.treesitter'), -- Provides a simple and easy way to use the interface for tree-sitter
     require('plugins.nvim_ufo'), -- Makes Neovim's fold look modern and keep high performance
-    { 'numToStr/Comment.nvim', opts = {} }, -- Smart and Powerful commenting plugin for neovim
 
     -- Navigation
-    require('plugins.tree'), -- A File Explorer For Neovim
-    require('plugins.bufferline'), -- A snazzy buffer line (with tabpage integration) for Neovim built using lua
     { 'theprimeagen/harpoon', lazy = true }, -- Getting you where you want with the fewest keystrokes
-    require('plugins.lualine'), -- A blazing fast and easy to configure Neovim statusline
-    require('plugins.which_key'), -- Displays a popup with possible key bindings
     { 'mbbill/undotree' }, -- Visualizes the undo history and makes it easy to browse and switch between different undo branches
     { 'moll/vim-bbye' }, -- Allows you to do delete buffers (close files) without closing your windows or messing up your layout
+    require('plugins.tree'), -- A File Explorer For Neovim
+    require('plugins.bufferline'), -- A snazzy buffer line (with tabpage integration) for Neovim built using lua
+    require('plugins.lualine'), -- A blazing fast and easy to configure Neovim statusline
+    require('plugins.which_key'), -- Displays a popup with possible key bindings
 
     -- Utils
     { 'williamboman/mason.nvim', opts = {} }, -- Portable package manager for Neovim that runs everywhere Neovim runs
